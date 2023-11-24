c234e755b8a36a8d72e8570e68c4eb36d0e3742b
João Frugis <joaofrugis@gmail.com>
2023-11-24 11:16:07 -0300

    MongDB test

--- app/routes/predict.py
+++ app/routes/predict.py
@@ -1,8 +1,15 @@
 from fastapi import APIRouter
 from app.models.building import Building
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 
 router = APIRouter()
+database_uri = "mongodb+srv://joaofrugis:4293784*Jaum@goappcluster.u5qlawa.mongodb.net/?retryWrites=true&w=majority"
+client = MongoClient(database_uri, server_api=ServerApi('1'))
+db = client['property-valuation']
+collection = db['predict-data']
 
 @router.post("/predict")
 async def post_predict(building: Building):
+    collection.insert_one(building)
     return building
