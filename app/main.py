dbcdfe435ae0f319442de9cfc8602e6de44bef00
João Frugis <joaofrugis@gmail.com>
2023-12-22 10:31:46 -0300

    CORS configured

--- app/main.py
+++ app/main.py
@@ -1,6 +1,19 @@
 from fastapi import FastAPI
 from app.routes import predict
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    'https://www.elevengrandesobras.com.br/'
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
 
 app.include_router(predict.router)
