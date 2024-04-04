ad41bd1314c067ccd769ac13274648c0c52c67f5
João Frugis <joaofrugis@gmail.com>
2024-04-04 16:18:45 -0300

    MongoDB implemented

--- app/routes/predict.py
+++ app/routes/predict.py
@@ -1,15 +1,24 @@
 from fastapi import APIRouter, HTTPException
+import pymongo.server_api
 from app.models.building import Building
 from app.iamodels.pipeline import Pipeline
+import pymongo
 from typing import List
 
 router = APIRouter()
+uri = "mongodb+srv://joaofrugis:4293784@goappcluster.u5qlawa.mongodb.net/?retryWrites=true&w=majority&appName=GoAPPCluster"
+client = pymongo.MongoClient(uri, server_api=pymongo.server_api.ServerApi('1'))
+database = client["property-valuation"]
+predict_data = database["predict-data"]
 
-@router.post("/predict", response_model=List[float])
+@router.post("/predict", response_model=dict)
 async def post_predict(building: Building):
     pipeline = Pipeline()
-    try:        
-        response = pipeline.predict(building)
-        return [round(response[0],2)]
+    try:
+        response = round(pipeline.predict(building)[0],2)
+        building_dict = building.model_dump()
+        building_dict['result'] = response
+        _id = predict_data.insert_one(building_dict)
+        return {"result": response,"_id": str(_id.inserted_id)}
     except ValueError as e:
         return e
