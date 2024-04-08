063988d425023d2b3d8fac253928097305e9e74a
João Frugis <joaofrugis@gmail.com>
2024-04-08 15:03:19 -0300

    OPTIONS added to CORS config

--- app/main.py
+++ app/main.py
@@ -14,8 +14,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=['POST', 'OPTIONS'],
-    allow_headers=['Content-Type'],
+    allow_methods=['*'],
+    allow_headers=['*'],
 )
 
 app.include_router(predict.router)
