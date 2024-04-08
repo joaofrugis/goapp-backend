ac51365f47d65c4f6b0e87ed8c9497ac878e43ed
João Frugis <joaofrugis@gmail.com>
2024-04-08 15:01:23 -0300

    OPTIONS added to CORS config

--- app/main.py
+++ app/main.py
@@ -6,14 +6,14 @@ app = FastAPI()
 
 origins = [
     'https://www.elevengrandesobras.com.br/',
-    'https://editor.wix.com/'
+    'https://editor.wix.com/',
+    '*'
 ]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_origins=["*"],
     allow_methods=['POST', 'OPTIONS'],
     allow_headers=['Content-Type'],
 )
